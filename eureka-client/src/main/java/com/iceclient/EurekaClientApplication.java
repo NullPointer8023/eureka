8ac71b66673e0bb33d0e38012472b0dd2e6734f3
周冰洁 <zhoubingjie@allinmd.cn>
2018-11-14 15:23:35 +0800

    three eureka server、one eureka client

---
+++ eureka-client/src/main/java/com/iceclient/EurekaClientApplication.java
@@ -0,0 +1,18 @@
+package com.iceclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientApplication.class, args);
+	}
+}
---
+++ eureka-client/src/main/java/com/iceclient/controller/HiController.java
@@ -0,0 +1,23 @@
+package com.iceclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by allin on 2018/11/13.
+ */
+@RestController
+public class HiController {
+    @Value("${server.port}")
+    private String port ;
+
+    @GetMapping("/hi")
+    public String home(@RequestParam String name ){
+        return "hi + " + name + ",i am from port:" + port ;
+    }
+
+}
